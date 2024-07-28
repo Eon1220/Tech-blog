a9002d6d05e239e4a40b3b311f70e826748d41cb
Eon Pace <nicolaspace21@gmail.com>
2024-07-28 12:19:37 -0400

    Adds public code, improves js and css

---
+++ public/js/profile.js
@@ -0,0 +1,76 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#blog-title').value.trim();
+    const body = document.querySelector('#blog-body').value.trim();
+  
+    if (title && body) {
+      const response = await fetch(`/api/blogs`, {
+        method: 'POST',
+        body: JSON.stringify({ title, body }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create blogpost');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogs/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  const editButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const title = document.querySelector('#blog-title').value.trim();
+      const body = document.querySelector('#blog-body').value.trim();
+      const id = event.target.getAttribute('data-id');
+      if (title && body) {
+        const response = await fetch(`/api/blogs/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ title, body }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        // const response = await fetch(`/api/blogs/${id}`, {
+        //   method: 'PUT',
+        // });
+  
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert('Failed to edit post');
+        }
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blog-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
+  
+  document
+    .querySelector('.blog-list')
+    .addEventListener('click', editButtonHandler);
